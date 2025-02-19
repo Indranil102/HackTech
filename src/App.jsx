0507bb474ae314a57020c93b5d5990ff9122c137
Indranil102 <indranilsamanta2003@gmail.com>
2025-02-19 19:17:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
         <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-        <div className='bg-gray'>
+        <div className='font-bold bg-gray-700 text-gray-50'>
           <p>changes by dp</p>
         </div>
     </>
