c194db83a3ad99f6e370d472f06cf0800de14037
Indranil102 <indranilsamanta2003@gmail.com>
2025-02-21 19:00:12 +0530

    update css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,23 +2,23 @@
 
 const Navbar = () => {
   return (
-    <div className="h-[70px] w-full bg-blue-500 text-white flex">
+    <div className="h-[70px] w-full  text-black flex shadow-lg">
       
       <div className="ml-[100px] flex items-center space-x-2">
         <img className="" src="" alt="logo" />
       <h2 className="ml-4">Insight Hunts</h2>
       
       
-      <ul className="flex space-x-4 ml-[40px]">
+      <ul className="flex space-x-4 ml-[40px] font-medium cursor-pointer">
       <li>DashBoard</li>
           <li>Analytic </li>
           <li>Report</li>
           
       </ul>
       </div>
-      <div className="flex items-center space-x-4 ml-[700px]">
+      <div className="flex items-center space-x-4 ml-[700px] cursor-pointer">
         <img src="" alt="icon" />
-        <button className="">Login</button>
+        <button className="w-[90px] h-[40px] bg-blue-800 rounded text-xl text-amber-50">Login</button>
         
          
         
