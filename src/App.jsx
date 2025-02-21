efeb7147f49d4c45423814cdf9b335eea585a985
Indranil102 <indranilsamanta2003@gmail.com>
2025-02-21 17:58:43 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 
+
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
-       <Navbar/>
-=======
-        <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-        <div className='font-bold bg-gray-700 text-gray-50'>
-          <p>changes by dp</p>
-        </div>
-        <p>Manav</p>
->>>>>>> 80535e4428e3fb0317efc7b17aa16632faad4e4c
+      <Navbar />
     </>
   )
 }
